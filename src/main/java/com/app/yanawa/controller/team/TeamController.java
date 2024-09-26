e7b76ba92576ea9926aac12498605b81ae93bb43
000jisoo <000jisoo@gmail.com>
2024-09-26 16:29:07 +0900

    jjs

--- src/main/java/com/app/yanawa/controller/team/TeamController.java
+++ src/main/java/com/app/yanawa/controller/team/TeamController.java
@@ -1,9 +1,11 @@
 package com.app.yanawa.controller.team;
 
+import com.app.yanawa.service.member.MemberService;
 import com.app.yanawa.service.team.TeamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class TeamController {
     private final TeamService teamService;
+    private final MemberService memberService;
+
+    @GetMapping("")
 }
