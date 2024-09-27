f2ab8fe3c60657c456e0defc333cbc3fdd233513
000jisoo <000jisoo@gmail.com>
2024-09-27 17:09:04 +0900

    jjs

--- src/main/java/com/app/yanawa/controller/team/TeamMemberController.java
+++ src/main/java/com/app/yanawa/controller/team/TeamMemberController.java
@@ -21,15 +21,25 @@ import org.springframework.web.servlet.view.RedirectView;
 public class TeamMemberController {
     private final TeamMemberService teamMemberService;
     private final MemberService memberService;
+    private final TeamService teamService;
 
     @GetMapping("teamRecruitDetail")
     public void goToWriteFrom(Model model) {
         model.addAttribute("member", memberService.getMember(7L).get());
+        model.addAttribute("team", teamService.getTeam(8L).get());
+        log.info("GetMapping 들어옴");
     }
 
     @PostMapping("teamRecruitDetail")
     public RedirectView write(TeamMemberDTO teamMemberDTO) {
+        log.info("PostMapping 1 들어옴");
+        // pathToContact를 int로 변환
+//        int pathToContact = Integer.parseInt(teamMemberDTO.getPathToContact()); // String에서 int로 변환
+//        teamMemberDTO.setPathToContact(pathToContact); // 변환한 값을 DTO에 설정
         teamMemberService.join(teamMemberDTO.toVO());
-        return new RedirectView("/teamRecruit/teamCreate");
+        log.info("PostMapping 2 들어옴");
+        log.info("{}", teamMemberDTO);
+        log.info(teamMemberDTO.toString());
+        return new RedirectView("/teamRecruit/teamRecruitDetail");
     }
 }
--- src/main/resources/static/js/teamrecruit_page/teamrecruitDetailPage.js
+++ src/main/resources/static/js/teamrecruit_page/teamrecruitDetailPage.js
@@ -23,17 +23,17 @@ btnBackgroundColor.addEventListener("mouseout", function () {
 
 // -------------------------------------------------------------------
 
-// .sider-1안에 있는 신청하기버튼
-const applyButton = document.querySelector(".side-match-btn");
-
-// .sider-2안에 있는 x 이미지(신청하기 닫기)
-const applyCloseButton = document.querySelector(".apply-close-btn");
-
-// .sider-2안에 있는 신청완료 버튼누르기 (신청완료됨)
-const applyCompleteButton = document.querySelector(".apply-complete-btn");
-
-// 신청하기와 유사한 추천매칭팀 div
-const sideDiv1 = document.querySelector(".sider-1");
+// // .sider-1안에 있는 신청하기버튼
+// const applyButton = document.querySelector(".side-match-btn");
+//
+// // .sider-2안에 있는 x 이미지(신청하기 닫기)
+// const applyCloseButton = document.querySelector(".apply-close-btn");
+//
+// // .sider-2안에 있는 신청완료 버튼누르기 (신청완료됨)
+// const applyCompleteButton = document.querySelector(".apply-complete-btn");
+//
+// // 신청하기와 유사한 추천매칭팀 div
+// const sideDiv1 = document.querySelector(".sider-1");
 
 // // 신청하기 신청상세 div
 // const sideDiv2 = document.querySelector(".sider-2");
@@ -41,17 +41,17 @@ const sideDiv1 = document.querySelector(".sider-1");
 // // 신청완료시 신청완료 완료 됬다는 div
 // const sideDiv3 = document.querySelector(".sider-3");
 
-// 신청하기 버튼누르면 신청 div나옴 페이지이동 X
-applyButton.addEventListener("click", () => {
-    sideDiv1.style.display = "none";
-    sideDiv2.style.display = "block";
-});
-
-// x 이미지 누르면 초기화면 나옴 페이지이동 X
-applyCloseButton.addEventListener("click", () => {
-    sideDiv1.style.display = "block";
-    sideDiv2.style.display = "none";
-});
+// // 신청하기 버튼누르면 신청 div나옴 페이지이동 X
+// applyButton.addEventListener("click", () => {
+//     sideDiv1.style.display = "none";
+//     sideDiv2.style.display = "block";
+// });
+//
+// // x 이미지 누르면 초기화면 나옴 페이지이동 X
+// applyCloseButton.addEventListener("click", () => {
+//     sideDiv1.style.display = "block";
+//     sideDiv2.style.display = "none";
+// });
 
 // // 신청완료 버튼 누를시 신청완료 div나옴 페이지이동 X
 // applyCompleteButton.addEventListener("click", () => {
@@ -64,7 +64,7 @@ applyCloseButton.addEventListener("click", () => {
 // 팀 정보보기 버튼
 const teamInfoButton = document.querySelector(".team-ifo-btn");
 
-// 버튼 누를시 팀 정보 상세페이지로 이동 (추후에 변동)
-teamInfoButton.addEventListener("click", () => {
-    alert("팀정보를 불러옵니다.");
-});
+// // 버튼 누를시 팀 정보 상세페이지로 이동 (추후에 변동)
+// teamInfoButton.addEventListener("click", () => {
+//     alert("팀정보를 불러옵니다.");
+// });
