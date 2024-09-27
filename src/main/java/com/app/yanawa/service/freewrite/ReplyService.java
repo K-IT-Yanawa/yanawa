6f23d3095e926b7bd7f1c413d4fe4fc36e754361
guswnsdldl <guswnsdldl@naver.com>
2024-09-27 16:55:17 +0900

    조회성공

--- src/main/java/com/app/yanawa/service/freewrite/ReplyService.java
+++ src/main/java/com/app/yanawa/service/freewrite/ReplyService.java
@@ -1,4 +1,20 @@
 package com.app.yanawa.service.freewrite;
 
+import com.app.yanawa.domain.freewrite.ReplyDTO;
+import com.app.yanawa.domain.freewrite.ReplyVO;
+
+import java.util.List;
+
 public interface ReplyService {
+    // 댓글 작성
+    void write(ReplyVO replyVO);
+
+    // 특정 게시물의 댓글 목록 조회
+    List<ReplyDTO> getRepliesByPostId(Long postId);
+
+    // 댓글 수정
+    void update(ReplyVO replyVO);
+
+    // 댓글 삭제
+    void delete(Long id);
 }
