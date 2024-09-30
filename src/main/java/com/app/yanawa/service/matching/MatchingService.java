5b902bc9778c1d9bd42dec6e28db95449a677e66
ChoiDh05 <topaz7107@gmail.com>
2024-09-30 14:35:39 +0900

    list add from cdh

--- src/main/java/com/app/yanawa/service/matching/MatchingService.java
+++ src/main/java/com/app/yanawa/service/matching/MatchingService.java
@@ -1,15 +1,28 @@
 package com.app.yanawa.service.matching;
 
 
+import com.app.yanawa.domain.matching.Pagination;
 import com.app.yanawa.domain.matching.MatchingDTO;
-import com.app.yanawa.domain.matching.MatchingVO;
+import com.app.yanawa.domain.member.MemberVO;
+import com.app.yanawa.domain.team.TeamVO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MatchingService {
     public void write(MatchingDTO matchingDTO);
+    public Optional<MemberVO> getMember(Long id);
+    public Optional<TeamVO> getTeam(Long id);
 
-    public List<MatchingDTO> getListMatching();
 
-    public int getTotalMatching();
+    Optional<MatchingDTO> getMatchingInfoByMemberId(Long id);
+
+    public List<MatchingDTO> getListMatching(Pagination pagination, String order, String sport, String place, String time);
+
+    public int getTotalMatching(String sport, String place, String time);
+
+    public int getMatchingCount();
+
+
+
 }
